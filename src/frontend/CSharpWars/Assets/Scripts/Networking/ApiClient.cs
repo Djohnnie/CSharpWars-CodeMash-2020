8cf3abc48ad993cc2705b8f134238c763449751c
Johnny Hooyberghs <7031043+Djohnnie@users.noreply.github.com>
2020-01-08 00:09:10 +0100

    ApiClient

--- src/frontend/CSharpWars/Assets/Scripts/Networking/ApiClient.cs
+++ src/frontend/CSharpWars/Assets/Scripts/Networking/ApiClient.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using Assets.Scripts.Model;
-using RestSharp;
-
-namespace Assets.Scripts.Networking
+﻿namespace Assets.Scripts.Networking
 {
     public static class ApiClient
     {
